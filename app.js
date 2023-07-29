5e2e983a1f5cf0d9a177a050f94f120c2fb3d9cb
ahmed-faraz123 <131711133+ahmed-faraz123@users.noreply.github.com>
2023-07-29 21:44:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var imgList = document.getElementById("img-list");
+var caroBtns = document.getElementsByClassName("caro-btn");
+
+var status = 0;
+var positionUit = -100;
+
+function slideImg(x) {
+    var i;
+    for (i=0;i<caroBtns.length; i++) {
+
+        caroBtns[i].style.backgroundColor = "#fffff44";
+
+    }
+    caroBtns[x].style.backgroundColor = "#fff";
+    position = x * positionUit;
+    imgList.style.left = position + "%";
+}
